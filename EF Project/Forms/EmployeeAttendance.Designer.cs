488fcf0dea34d0fd3329170fd793858f49d1c759
AhmedAbuAlmagd <abuelmagdahmed6@gmail.com>
2025-03-13 06:10:54 +0200

    Adding QR

--- EF Project/Forms/EmployeeAttendance.Designer.cs
+++ EF Project/Forms/EmployeeAttendance.Designer.cs
@@ -34,22 +34,25 @@
             dgv_empattend_EAF = new DataGridView();
             btn_back_EAF = new Button();
             date_EAF = new DateTimePicker();
+            pbQrCode = new PictureBox();
+            btn_showbyuseranddate_EAF = new Button();
             ((System.ComponentModel.ISupportInitialize)dgv_empattend_EAF).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbQrCode).BeginInit();
             SuspendLayout();
             // 
             // cb_showbyuser_EAF
             // 
             cb_showbyuser_EAF.FormattingEnabled = true;
-            cb_showbyuser_EAF.Location = new Point(686, 80);
+            cb_showbyuser_EAF.Location = new Point(686, 47);
             cb_showbyuser_EAF.Name = "cb_showbyuser_EAF";
             cb_showbyuser_EAF.Size = new Size(151, 28);
             cb_showbyuser_EAF.TabIndex = 8;
             // 
             // btn_showbydate_EAF
             // 
-            btn_showbydate_EAF.Location = new Point(518, 180);
+            btn_showbydate_EAF.Location = new Point(508, 147);
             btn_showbydate_EAF.Name = "btn_showbydate_EAF";
-            btn_showbydate_EAF.Size = new Size(134, 40);
+            btn_showbydate_EAF.Size = new Size(153, 40);
             btn_showbydate_EAF.TabIndex = 7;
             btn_showbydate_EAF.Text = "Show By Date";
             btn_showbydate_EAF.UseVisualStyleBackColor = true;
@@ -57,11 +60,11 @@
             // 
             // btn_showbyuser_EAF
             // 
-            btn_showbyuser_EAF.Location = new Point(518, 73);
+            btn_showbyuser_EAF.Location = new Point(508, 40);
             btn_showbyuser_EAF.Name = "btn_showbyuser_EAF";
-            btn_showbyuser_EAF.Size = new Size(134, 40);
+            btn_showbyuser_EAF.Size = new Size(153, 40);
             btn_showbyuser_EAF.TabIndex = 6;
-            btn_showbyuser_EAF.Text = "Show By User";
+            btn_showbyuser_EAF.Text = "Show By Employee";
             btn_showbyuser_EAF.UseVisualStyleBackColor = true;
             btn_showbyuser_EAF.Click += btn_showbyuser_EAF_Click;
             // 
@@ -73,6 +76,7 @@
             dgv_empattend_EAF.RowHeadersWidth = 51;
             dgv_empattend_EAF.Size = new Size(487, 411);
             dgv_empattend_EAF.TabIndex = 5;
+            dgv_empattend_EAF.RowHeaderMouseDoubleClick += dgv_empattend_EAF_RowHeaderMouseDoubleClick;
             // 
             // btn_back_EAF
             // 
@@ -87,16 +91,37 @@
             // date_EAF
             // 
             date_EAF.Format = DateTimePickerFormat.Short;
-            date_EAF.Location = new Point(686, 185);
+            date_EAF.Location = new Point(686, 152);
             date_EAF.Name = "date_EAF";
             date_EAF.Size = new Size(151, 27);
             date_EAF.TabIndex = 11;
+            // 
+            // pbQrCode
+            // 
+            pbQrCode.Location = new Point(587, 289);
+            pbQrCode.Name = "pbQrCode";
+            pbQrCode.Size = new Size(200, 200);
+            pbQrCode.SizeMode = PictureBoxSizeMode.Zoom;
+            pbQrCode.TabIndex = 13;
+            pbQrCode.TabStop = false;
+            // 
+            // btn_showbyuseranddate_EAF
+            // 
+            btn_showbyuseranddate_EAF.Location = new Point(572, 218);
+            btn_showbyuseranddate_EAF.Name = "btn_showbyuseranddate_EAF";
+            btn_showbyuseranddate_EAF.Size = new Size(229, 40);
+            btn_showbyuseranddate_EAF.TabIndex = 14;
+            btn_showbyuseranddate_EAF.Text = "Show by Employee And Date";
+            btn_showbyuseranddate_EAF.UseVisualStyleBackColor = true;
+            btn_showbyuseranddate_EAF.Click += btn_showbyuseranddate_EAF_Click;
             // 
             // EmployeeAttendance
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(859, 534);
+            Controls.Add(btn_showbyuseranddate_EAF);
+            Controls.Add(pbQrCode);
             Controls.Add(date_EAF);
             Controls.Add(btn_back_EAF);
             Controls.Add(cb_showbyuser_EAF);
@@ -106,6 +131,7 @@
             Name = "EmployeeAttendance";
             Text = "EmployeeAttendance";
             ((System.ComponentModel.ISupportInitialize)dgv_empattend_EAF).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbQrCode).EndInit();
             ResumeLayout(false);
         }
 
@@ -116,5 +142,7 @@
         private DataGridView dgv_empattend_EAF;
         private Button btn_back_EAF;
         private DateTimePicker date_EAF;
+        private PictureBox pbQrCode;
+        private Button btn_showbyuseranddate_EAF;
     }
 }
--- EF Project/Forms/EmployeeAttendance.cs
+++ EF Project/Forms/EmployeeAttendance.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 using EmployeeAttendanceSystem.BusinessLogic.Services;
 using EmployeeAttendanceSystem.DataAccess.Context;
 using EmployeeAttendanceSystem.Models;
+using QRCoder;
+using System.Drawing;
+using Newtonsoft.Json;
+using EmployeeAttendanceSystem.DataAccess.Models;
 
 namespace EF_Project.Forms
 {
@@ -20,7 +24,7 @@ namespace EF_Project.Forms
         EmployeeServices employeeServices;
         Form prevForm;
         int employee_id;
-        public EmployeeAttendance(int employee_id,Form prevForm)
+        public EmployeeAttendance(int employee_id, Form prevForm)
         {
             InitializeComponent();
             this.context = new AttendanceContext();
@@ -29,29 +33,18 @@ namespace EF_Project.Forms
             this.prevForm = prevForm;
             this.employee_id = employee_id;
             LoadEmployees();
-            LoadDates();
         }
         private void LoadEmployees()
         {
             List<Employee> employees = new List<Employee>();
-            if(employeeServices.GetById(employee_id).User.role.ToString() == "HR")
-              employees = employeeServices.GetAllEmp();
+            if (employeeServices.GetById(employee_id).User.role.ToString() == "HR")
+                employees = employeeServices.GetAllEmp();
             else
-              employees = employeeServices.GetAllEmpAndHr();
+                employees = employeeServices.GetAllEmpAndHr();
 
             cb_showbyuser_EAF.DataSource = employees;
             cb_showbyuser_EAF.DisplayMember = "name";
             cb_showbyuser_EAF.ValueMember = "id";
-        }
-        private void LoadDates()
-        {
-            var dates = attendanceServices.GetAllDates();
-            if(dates == null || dates.Count == 0)
-            {
-                MessageBox.Show("No Dates Recorded", "warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            cb_showbydate_EAF.DataSource = dates;
         }
 
 
@@ -72,12 +65,79 @@ namespace EF_Project.Forms
             var attendanceList = attendanceServices.GetAttendanceByDate(selectedDate);
             dgv_empattend_EAF.DataSource = attendanceList;
             dgv_empattend_EAF.Columns["Employee"].Visible = false;
+
         }
 
         private void btn_back_EAF_Click(object sender, EventArgs e)
         {
             this.Close();
             prevForm.Show();
+        }
+
+        private string GenerateQrData(List<Attendance> attendanceList)
+        {
+            if (attendanceList == null || !attendanceList.Any())
+            {
+                return "No attendance data available";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var record in attendanceList.Take(5))
+            {
+                sb.AppendLine($"Employee: {record.Employee.name}");
+                sb.AppendLine($"Date: {record.Date.ToString("yyyy-MM-dd")}");
+                sb.AppendLine($"Check In: {record.checkInTime}");
+                sb.AppendLine($"Check Out: {record.checkOutTime}");
+                sb.AppendLine($"Working Hours: {record.WorkingHours}");
+                sb.AppendLine($"Status: {record.attendanceStatus}");
+                sb.AppendLine($"Late: {(record.IsLate ? "Yes" : "No")}");
+                sb.AppendLine($"Early Departure: {(record.IsEarlyDeparture ? "Yes" : "No")}");
+                sb.AppendLine("-------------------------------------------------------");
+            }
+            return sb.ToString();
+        }
+
+        private Bitmap GenerateQrCode(string data)
+        {
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
+        }
+
+        private void btn_showbyuseranddate_EAF_Click(object sender, EventArgs e)
+        {
+            if (cb_showbyuser_EAF.SelectedValue != null)
+            {
+                int employeeId = (int)cb_showbyuser_EAF.SelectedValue;
+                DateOnly selectedDate = DateOnly.FromDateTime(date_EAF.Value.Date);
+
+                var attendanceList = attendanceServices.GetAttendanceByEmpIdAndDate(employeeId, selectedDate);
+
+                dgv_empattend_EAF.DataSource = attendanceList;
+                dgv_empattend_EAF.Columns["Employee"].Visible = false;
+
+                string qrData = GenerateQrData(attendanceList);
+                Bitmap qrCode = GenerateQrCode(qrData);
+                pbQrCode.Image = qrCode;
+            }
+            else
+            {
+                MessageBox.Show("Please select an employee.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void dgv_empattend_EAF_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int employeeId = (int)dgv_empattend_EAF.SelectedRows[0].Cells["Employee_id"].Value ;
+            DateOnly selectedDate = (DateOnly)dgv_empattend_EAF.SelectedRows[0].Cells["Date"].Value;
+            var attendanceList = attendanceServices.GetAttendanceByEmpIdAndDate(employeeId, selectedDate);
+
+            string qrData = GenerateQrData(attendanceList);
+            Bitmap qrCode = GenerateQrCode(qrData);
+            pbQrCode.Image = qrCode;
+
         }
     }
 }
