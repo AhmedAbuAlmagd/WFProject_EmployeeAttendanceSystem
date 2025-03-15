8a613a310ab17b08c6e88e43161c3e1962706192
AhmedAbuAlmagd <abuelmagdahmed6@gmail.com>
2025-03-15 15:11:19 +0200

    Complete UI of Forms

--- EF Project/Forms/AdminDashboard.Designer.cs
+++ EF Project/Forms/AdminDashboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboard));
             btn_logout_HRDF = new Button();
             btn_showreport_HRF = new Button();
             btn_showrequest_HRF = new Button();
@@ -36,93 +37,130 @@
             btn_usersManagement_HRD = new Button();
             btn_deptsManagement_HRF = new Button();
             btn_showlogs_ADF = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btn_logout_HRDF
             // 
-            btn_logout_HRDF.Location = new Point(697, 558);
+            btn_logout_HRDF.BackColor = Color.FromArgb(75, 75, 75);
+            btn_logout_HRDF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_logout_HRDF.ForeColor = SystemColors.ControlLightLight;
+            btn_logout_HRDF.Location = new Point(737, 488);
             btn_logout_HRDF.Name = "btn_logout_HRDF";
-            btn_logout_HRDF.Size = new Size(137, 50);
+            btn_logout_HRDF.Size = new Size(116, 44);
             btn_logout_HRDF.TabIndex = 13;
             btn_logout_HRDF.Text = "Log Out";
-            btn_logout_HRDF.UseVisualStyleBackColor = true;
+            btn_logout_HRDF.UseVisualStyleBackColor = false;
             btn_logout_HRDF.Click += btn_logout_HRDF_Click;
             // 
             // btn_showreport_HRF
             // 
-            btn_showreport_HRF.Location = new Point(214, 424);
+            btn_showreport_HRF.BackColor = Color.Teal;
+            btn_showreport_HRF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_showreport_HRF.ForeColor = SystemColors.ControlLightLight;
+            btn_showreport_HRF.Location = new Point(556, 344);
             btn_showreport_HRF.Name = "btn_showreport_HRF";
-            btn_showreport_HRF.Size = new Size(397, 57);
+            btn_showreport_HRF.Size = new Size(278, 50);
             btn_showreport_HRF.TabIndex = 12;
             btn_showreport_HRF.Text = "Show Reports";
-            btn_showreport_HRF.UseVisualStyleBackColor = true;
+            btn_showreport_HRF.UseVisualStyleBackColor = false;
             btn_showreport_HRF.Click += btn_showreport_HRF_Click;
             // 
             // btn_showrequest_HRF
             // 
-            btn_showrequest_HRF.Location = new Point(214, 339);
+            btn_showrequest_HRF.BackColor = Color.Teal;
+            btn_showrequest_HRF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_showrequest_HRF.ForeColor = SystemColors.ControlLightLight;
+            btn_showrequest_HRF.Location = new Point(556, 281);
             btn_showrequest_HRF.Name = "btn_showrequest_HRF";
-            btn_showrequest_HRF.Size = new Size(397, 57);
+            btn_showrequest_HRF.Size = new Size(278, 50);
             btn_showrequest_HRF.TabIndex = 11;
             btn_showrequest_HRF.Text = "Show Requests";
-            btn_showrequest_HRF.UseVisualStyleBackColor = true;
+            btn_showrequest_HRF.UseVisualStyleBackColor = false;
             btn_showrequest_HRF.Click += btn_showrequest_HRF_Click;
             // 
             // btn_empmanage_HRF
             // 
-            btn_empmanage_HRF.Location = new Point(214, 177);
+            btn_empmanage_HRF.BackColor = Color.Teal;
+            btn_empmanage_HRF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_empmanage_HRF.ForeColor = SystemColors.ControlLightLight;
+            btn_empmanage_HRF.Location = new Point(556, 155);
             btn_empmanage_HRF.Name = "btn_empmanage_HRF";
-            btn_empmanage_HRF.Size = new Size(397, 57);
+            btn_empmanage_HRF.Size = new Size(278, 50);
             btn_empmanage_HRF.TabIndex = 10;
             btn_empmanage_HRF.Text = "Employees Managment";
-            btn_empmanage_HRF.UseVisualStyleBackColor = true;
+            btn_empmanage_HRF.UseVisualStyleBackColor = false;
             btn_empmanage_HRF.Click += btn_empmanage_HRF_Click;
             // 
             // btn_empattend_HRF
             // 
-            btn_empattend_HRF.Location = new Point(214, 256);
+            btn_empattend_HRF.BackColor = Color.Teal;
+            btn_empattend_HRF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_empattend_HRF.ForeColor = SystemColors.ControlLightLight;
+            btn_empattend_HRF.Location = new Point(556, 218);
             btn_empattend_HRF.Name = "btn_empattend_HRF";
-            btn_empattend_HRF.Size = new Size(397, 57);
+            btn_empattend_HRF.Size = new Size(278, 50);
             btn_empattend_HRF.TabIndex = 9;
             btn_empattend_HRF.Text = "Employees Attendance";
-            btn_empattend_HRF.UseVisualStyleBackColor = true;
+            btn_empattend_HRF.UseVisualStyleBackColor = false;
             btn_empattend_HRF.Click += btn_empattend_HRF_Click;
             // 
             // btn_usersManagement_HRD
             // 
-            btn_usersManagement_HRD.Location = new Point(214, 98);
+            btn_usersManagement_HRD.BackColor = Color.Teal;
+            btn_usersManagement_HRD.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_usersManagement_HRD.ForeColor = SystemColors.ControlLightLight;
+            btn_usersManagement_HRD.Location = new Point(556, 92);
             btn_usersManagement_HRD.Name = "btn_usersManagement_HRD";
-            btn_usersManagement_HRD.Size = new Size(397, 57);
+            btn_usersManagement_HRD.Size = new Size(278, 50);
             btn_usersManagement_HRD.TabIndex = 7;
             btn_usersManagement_HRD.Text = "Users Management";
-            btn_usersManagement_HRD.UseVisualStyleBackColor = true;
+            btn_usersManagement_HRD.UseVisualStyleBackColor = false;
             btn_usersManagement_HRD.Click += btn_usersManagement_HRD_Click;
             // 
             // btn_deptsManagement_HRF
             // 
-            btn_deptsManagement_HRF.Location = new Point(214, 19);
+            btn_deptsManagement_HRF.BackColor = Color.Teal;
+            btn_deptsManagement_HRF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_deptsManagement_HRF.ForeColor = SystemColors.ControlLightLight;
+            btn_deptsManagement_HRF.Location = new Point(556, 29);
             btn_deptsManagement_HRF.Name = "btn_deptsManagement_HRF";
-            btn_deptsManagement_HRF.Size = new Size(397, 57);
+            btn_deptsManagement_HRF.Size = new Size(278, 50);
             btn_deptsManagement_HRF.TabIndex = 8;
             btn_deptsManagement_HRF.Text = "Departments Management ";
-            btn_deptsManagement_HRF.UseVisualStyleBackColor = true;
+            btn_deptsManagement_HRF.UseVisualStyleBackColor = false;
             btn_deptsManagement_HRF.Click += btn_deptsManagement_HRF_Click;
             // 
             // btn_showlogs_ADF
             // 
-            btn_showlogs_ADF.Location = new Point(214, 508);
+            btn_showlogs_ADF.BackColor = Color.Teal;
+            btn_showlogs_ADF.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            btn_showlogs_ADF.ForeColor = SystemColors.ControlLightLight;
+            btn_showlogs_ADF.Location = new Point(556, 407);
             btn_showlogs_ADF.Name = "btn_showlogs_ADF";
-            btn_showlogs_ADF.Size = new Size(397, 58);
+            btn_showlogs_ADF.Size = new Size(278, 50);
             btn_showlogs_ADF.TabIndex = 14;
             btn_showlogs_ADF.Text = "Show Logs";
-            btn_showlogs_ADF.UseVisualStyleBackColor = true;
+            btn_showlogs_ADF.UseVisualStyleBackColor = false;
             btn_showlogs_ADF.Click += btn_showlogs_ADF_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(-22, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(536, 544);
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
             // 
             // AdminDashboard
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(846, 634);
+            BackColor = Color.FromArgb(225, 223, 186);
+            ClientSize = new Size(865, 544);
+            Controls.Add(pictureBox1);
             Controls.Add(btn_showlogs_ADF);
             Controls.Add(btn_logout_HRDF);
             Controls.Add(btn_showreport_HRF);
@@ -133,6 +171,7 @@
             Controls.Add(btn_deptsManagement_HRF);
             Name = "AdminDashboard";
             Text = "AdminDashboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -146,5 +185,6 @@
         private Button btn_usersManagement_HRD;
         private Button btn_deptsManagement_HRF;
         private Button btn_showlogs_ADF;
+        private PictureBox pictureBox1;
     }
 }
